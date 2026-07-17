8fad55a8ff7542b0b7a6d54576a6322330179199
aldyymnlnk <riodynaoka@gmail.com>
2026-07-18 00:29:00 +0700

    update gitignore, untrack vercel output

--- .vercel/output/_functions/chunks/supabase_9w9KLbe1.mjs
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-var supabase = createClient("https://jixcpwfsgdahahjqesbu.supabase.co", "sb_publishable_GjnQtcdt3oJhODkf0ua_qg_Dt72dSfs");
-//#endregion
-export { supabase as t };
